3ed70a5fa7bafd63ba142da63cfe246d64bc3b84
Tristan Lostroh <tl8roy@gmail.com>
2015-06-10 18:02:35 +1000

    Adds folder and file manipulations to take photo sets

--- pi_photoscope/pi_photoscope/views.py
+++ pi_photoscope/pi_photoscope/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse
-import tempfile, zipfile
 from django.core.servers.basehttp import FileWrapper
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.conf import settings
-#from .models import Photos
 
-#import time
-#from time import sleep
+from os import listdir,mkdir
+from os.path import isfile, isdir, join
+import tempfile, zipfile
+
+import datetime
+from time import sleep
+
 from fractions import Fraction
+
+#from .models import Photos
 #import picamera
 
 def index(request):
@@ -59,15 +64,62 @@ def take(request):
             #sleep(10)"""
 
             # Take a preview Image
-            #camera.capture('preview.jpg')
+            #camera.capture(settings.ASTRO_IMAGES + 'preview.jpg')
         import base64
         image = settings.ASTRO_IMAGES+'preview.jpg'
         image_64 = base64.encodestring(open(image, "rb").read())
 
-        #Only take 1 photo and send it to a known location
+        #Process our actual sequence
         if request.POST['submit_btn'] == 'go':
-            pass
-            """camera.capture_sequence(['image%02d.jpg' % i for i in range(10)])"""
+            #Get all our astro folders
+            folders = [ f for f in listdir(settings.ASTRO_IMAGES) if isdir(join(settings.ASTRO_IMAGES,f)) ]
+            newFolderId = 1
+            makeADir = True
+            today = datetime.date.today().strftime('%Y-%m-%d')
+
+            #Go through each folder to see if we have today's
+            for folder in folders:
+                folderID = folder.split('_')
+                #If it is todays, skip the rest
+                if folderID[1] == today:
+                    newFolderId = folderID[0]
+                    makeADir = False
+                    break
+                elif newFolderId <= int(folderID[0]):
+                    #If the folder id is less than the current ID
+                    newFolderId = int(folderID[0]) + 1
+
+            #Set the current folder and make it if required
+            currentFolder = settings.ASTRO_IMAGES + str(newFolderId)+ '_' + today
+            if makeADir:
+                mkdir(currentFolder)
+
+            #Go through the folder to get the latest file id
+            newFileId = 1
+            files = [ f for f in listdir(currentFolder) if isfile(join(currentFolder,f)) ]
+            for file in files:
+                fileID = file.split('_')
+                if newFileId <= int(fileID[0].replace('.txt','')):
+                    newFileId = int(fileID[0].replace('.txt','')) + 1
+
+            #Write out a text file with the settings we used
+            f = open(currentFolder + '/' + str(newFileId) + '.txt','w+')
+            f.write('ID: ' + str(newFileId) + "\n")
+            f.write('Date: ' + str(datetime.datetime.now()) + "\n")
+            f.write('Name: ' + photo_parameters['name'] + "\n")
+            f.write('ISO: ' + str(photo_parameters['iso']) + "\n")
+            f.write('Shutter Speed:' + str(photo_parameters['shutter_speed']) + " s\n")
+            f.write('Shutter Speed Inverse: 1/' + str(photo_parameters['shutter_speed_inverse']) + "\n")
+            f.write('Number of Pictures: ' + str(photo_parameters['num_to_take']) + "\n")
+            f.write('Time Between: ' + str(photo_parameters['time_between']) + "\n")
+            f.close()
+
+            #Shoot the images
+            for i in range(1,photo_parameters['num_to_take'] + 1):
+                sleep(photo_parameters['time_between'])
+                f = open(currentFolder + '/' + str(newFileId) + '_' + str(i) +'.jpg','w+')
+                f.close()
+                #camera.capture(currentFolder + '/' + str(newFileId) + '_' + str(i) +'.jpg')
 
     return HttpResponse(render(request, 'pi_photoscope/take_photos.html', {
         'photo_parameters': photo_parameters,
